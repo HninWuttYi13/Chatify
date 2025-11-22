b55e9c8a16507d04c5570b98d67f50e77e8deed6
Hnin Wutt Yi <157496396+HninWuttYi13@users.noreply.github.com>
2025-11-22 21:39:17 +0630

    login and logout functions added successfully

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -53,3 +53,28 @@ export const signup = async(req, res)=> {
        res.status(500).json({message: "Internal server error"})
     }
 }
+export const login = async(req, res)=> {
+  const {email, password } = req.body;
+  if(!email || !password) return res.status(400).json({message: "email and password required"});
+  try {
+    const user = await User.findOne({email})
+    if(!user) return res.status(400).json({message: 'Invalid credentials'});
+    const checkPassword = await bcrypt.compare(password, user.password);
+    if(!checkPassword) return res.status(400).json({message: "Invalid credentials"})
+      generateToken(user._id, res);
+    res.status(200).json({
+      _id: user.id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic
+    })
+  } catch (error) {
+    console.error("Error in login controller", error);
+    res.status(500).json({message: "Internal server error"})
+  }
+}
+
+export const logout = async(_, res)=> {
+  res.cookie("jwt", "", {maxAge: 0})
+  res.status(200).json({message: "logout successfully"})
+}
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,12 +1,11 @@
 import express from "express";
-import { signup } from "../controllers/auth.controller.js";
+import { signup, login, logout } from "../controllers/auth.controller.js";
 const router = express.Router();
+
 router.post("/signup", signup);
-router.get("/login", async (__, res) => {
-  res.send("login endpoint");
-});
-router.get("/logout", async (__, res) => {
-  res.send("logout endpoint");
-});
+
+router.post("/login", login);
+
+router.post("/logout", logout);
 
 export default router;
