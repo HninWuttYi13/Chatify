a2d9bb2416a552d9b5ecfe740012f37612d6843b
Hnin Wutt Yi <157496396+HninWuttYi13@users.noreply.github.com>
2026-02-06 09:32:28 +0630

    changing port to deploy

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(cookieParser())
 app.use("/api/auth", authRouter);
 app.use("/api/messages", messageRouter);
 const PORT = process.env.PORT || 3000;
-server.listen(3000, () => {
+server.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
-  connectDB()
+  connectDB();
 });
